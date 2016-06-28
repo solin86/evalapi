831628ba932dc8c8daf2dc32dac389e1588f8eb5
Benjamin Metzger <benjamin@flaregames.com>
2016-06-28 13:45:58 +0200

    Add dummy tf files

---
+++ evalapi-stack.tf
@@ -0,0 +1,13 @@
+resource "aws_opsworks_stack" "stack" {
+  name = "evalapi-staging"
+  region = "us-east-1"
+  service_role_arn = "arn:aws:iam::794759568553:role/aws-opsworks-service-role"
+  default_instance_profile_arn = "arn:aws:iam::794759568553:instance-profile/aws-opsworks-ec2-role"
+  agent_version = "LATEST"
+  default_availability_zone = "us-east-1a"
+  configuration_manager_name = "Chef"
+  configuration_manager_version = "11.10"
+  use_custom_cookbooks = true
+  use_opsworks_security_groups = true
+  default_os = "Ubuntu 14.04 LTS"
+}
